2e6837ccd677eae4e600ae7cea331d5c6a28d04b
prabhavpandey <prabhavp380@gmail.com>
2026-03-20 10:53:34 +0530

    fix: serverless crash due to logger file transport

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,6 +1,29 @@
 const winston = require('winston');
 const path = require('path');
 
+const transports = [
+  new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    )
+  })
+];
+
+// Only write logs to files if NOT running on Vercel
+// Vercel has a read-only filesystem (except /tmp) and captures stdout automatically
+if (!process.env.VERCEL) {
+  transports.push(
+    new winston.transports.File({ 
+      filename: path.join(__dirname, '../../logs/error.log'), 
+      level: 'error' 
+    }),
+    new winston.transports.File({ 
+      filename: path.join(__dirname, '../../logs/combined.log') 
+    })
+  );
+}
+
 const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || 'info',
   format: winston.format.combine(
@@ -8,21 +31,7 @@ const logger = winston.createLogger({
     winston.format.json()
   ),
   defaultMeta: { service: 'talwinder-bot' },
-  transports: [
-    new winston.transports.File({ 
-      filename: path.join(__dirname, '../../logs/error.log'), 
-      level: 'error' 
-    }),
-    new winston.transports.File({ 
-      filename: path.join(__dirname, '../../logs/combined.log') 
-    }),
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.simple()
-      )
-    })
-  ]
+  transports: transports
 });
 
 module.exports = logger;
